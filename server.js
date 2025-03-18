3539cca5d3745a64c2d0b034a159af47bb97f43d
Shoham Shtiler <shtilershoham@gmail.com>
2025-03-18 22:08:03 +0200

    Add  API for Bugs CRUDL

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
 import express from 'express'
+import { bugService } from './services/bugService.js'
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => res.send('Hello there'))
-app.listen(3030, () => console.log('Server ready at port 3030'))
+app.get('/api/bug', (req, res) => {
+    bugService.query().then(bugs => res.json(bugs))
+})
+
+app.get('/api/bug/save', (req, res) => {
+    const bug = req.query
+    if (bug.severity) bug.severity = +bug.severity
+    bugService.save(bug).then(savedBug => res.json(savedBug))
+})
+
+app.get('/api/bug/:bugId', (req, res) => {
+    const bugId = req.params.bugId
+    bugService.getById(bugId).then(bug => {
+        if (!bug) return res.status(404).send('Bug not found')
+        res.json(bug)
+    })
+})
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const bugId = req.params.bugId
+    bugService.remove(bugId).then(() => res.send('Bug removed'))
+})
+
+app.get('/', (req, res) => {
+    res.send('Bug API is running 🚀')
+})
+
+
+app.listen(3030, () => {
+    console.log('Server ready at port 3030')
+})
---
+++ services/bugService.js
@@ -0,0 +1,51 @@
+import { utilService } from "./util.service.js"
+import fs from 'fs'
+
+const bugs = utilService.readJsonFile('data/bug.json')
+
+export const bugService = {
+    query,
+    getById,
+    remove,
+    save
+}
+
+function query() {
+    return Promise.resolve(bugs)
+}
+
+function getById(bugId) {
+    const bug = bugs.find(bug => bug._id === bugId)
+    if (!bug) return Promise.reject('Cannot find bug - ' + bugId)
+    return Promise.resolve(bug)
+}
+
+function remove(bugId) {
+    const idx = bugs.findIndex(bug => bug._id === bugId)
+    if (idx === -1) return Promise.reject('Cannot remove bug - ' + bugId)
+    bugs.splice(idx, 1)
+    return _saveBugsToFile()
+}
+
+function save(bugToSave) {
+    if (bugToSave._id) {
+        const idx = bugs.findIndex(bug => bug._id === bugToSave._id)
+        bugs[idx] = bugToSave
+    } else {
+        bugToSave._id = utilService.makeId()
+        bugToSave.createdAt = Date.now()
+        bugs.unshift(bugToSave)
+    }
+
+    return _saveBugsToFile().then(() => bugToSave)
+}
+
+function _saveBugsToFile() {
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(bugs, null, 4)
+        fs.writeFile('data/bug.json', data, (err) => {
+            if (err) return reject(err)
+            resolve()
+        })
+    })
+}
