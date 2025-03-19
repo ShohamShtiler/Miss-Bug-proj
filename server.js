a03dab1c5bca25415c73bede99d2b45dcfa5dc5d
Shoham Shtiler <shtilershoham@gmail.com>
2025-03-19 21:52:45 +0200

    Refactor the bug.service.local.js  to use my API instead of using localStorage

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import express from 'express'
 import { bugService } from './services/bugService.js'
 
 const app = express()
+
 app.use(express.json())
+app.use(express.static('public'))
 
 app.get('/api/bug', (req, res) => {
     bugService.query().then(bugs => res.json(bugs))
